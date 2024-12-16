722da218aaa03b0dd0e356518772ab5b23d8d778
Natalia Bandurova <natacsp@mail.ru>
2024-12-16 13:09:03 +0300

    chore: install taiga ui

--- code-editor/src/app/app.component.ts
+++ code-editor/src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
+    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
 export class AppComponent {
   title = 'code-editor';
