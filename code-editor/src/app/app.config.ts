c066b068bbfbb756fe42fe239fcdfe542c75d2d2
Natalia Bandurova <natacsp@mail.ru>
2024-12-17 13:36:30 +0300

    feat: add code-editor layout

--- code-editor/src/app/app.config.ts
+++ code-editor/src/app/app.config.ts
@@ -1,14 +1,25 @@
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { TuiRootModule } from "@taiga-ui/core";
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
+import {
+    ApplicationConfig,
+    provideZoneChangeDetection,
+    importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { NGX_MONACO_EDITOR_CONFIG } from 'ngx-monaco-editor-v2';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
+    providers: [
         provideAnimations(),
         provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-        importProvidersFrom(TuiRootModule)
+        provideRouter(appRoutes),
+        importProvidersFrom(TuiRootModule),
+        {
+            provide: NGX_MONACO_EDITOR_CONFIG,
+            useValue: {
+                baseUrl: 'assets/monaco',
+            },
+        },
     ],
 };
--- code-editor/src/app/code-editor/code-editor.component.ts
+++ code-editor/src/app/code-editor/code-editor.component.ts
@@ -1,13 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
     standalone: true,
     selector: 'app-code-editor',
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, MonacoEditorModule, TuiButtonModule],
     templateUrl: './code-editor.component.html',
     styleUrl: './code-editor.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CodeEditorComponent {}
+export class CodeEditorComponent {
+    language = 'javascript';
+    code = '// Write your code here!';
+    output = '';
+    editorOptions = { theme: 'vs-dark', language: this.language };
+
+    runCode() {
+        console.log('run');
+    }
+}
