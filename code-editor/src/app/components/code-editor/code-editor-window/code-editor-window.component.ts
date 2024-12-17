b1b23058899320a3dcac5c828da9b57fa920b046
Natalia Bandurova <natacsp@mail.ru>
2024-12-17 18:54:11 +0300

    feat: add reesponse

--- code-editor/src/app/components/code-editor/code-editor-window/code-editor-window.component.ts
+++ code-editor/src/app/components/code-editor/code-editor-window/code-editor-window.component.ts
@@ -1,7 +1,7 @@
 import {
     ChangeDetectionStrategy,
     Component,
-    inject,
+    // inject,
     OnChanges,
     OnInit,
     SimpleChanges,
@@ -20,8 +20,8 @@ import {
     TuiDataListModule,
 } from '@taiga-ui/core';
 import { TuiDataListWrapperModule, TuiSelectModule } from '@taiga-ui/kit';
-import { HttpClient } from '@angular/common/http';
-import { LANGUAGES } from '../../../constants/languages';
+// import { HttpClient } from '@angular/common/http';
+// import { LANGUAGES } from '../../../constants/languages';
 
 @Component({
     standalone: true,
@@ -43,9 +43,10 @@ import { LANGUAGES } from '../../../constants/languages';
 })
 export class CodeEditorWindowComponent implements OnChanges, OnInit {
     code = 'function x() {\nconsole.log("Hello world!");\n}';
-    output = '';
+    output: string | null = null;
+    error: string | null = null;
 
-    languageControl = new FormControl(LANGUAGES.javascript);
+    languageControl = new FormControl<'javascript' | 'python'>('javascript');
     formGroup = new FormGroup({
         language: this.languageControl,
     });
@@ -56,8 +57,8 @@ export class CodeEditorWindowComponent implements OnChanges, OnInit {
         automaticLayout: true,
     };
 
-    private readonly http = inject(HttpClient);
-    readonly languages = Object.values(LANGUAGES);
+    // private readonly http = inject(HttpClient);
+    readonly languages = ['javascript', 'python'];
 
     ngOnInit() {
         this.languageControl.valueChanges.subscribe((language) => {
@@ -74,14 +75,60 @@ export class CodeEditorWindowComponent implements OnChanges, OnInit {
     runCode() {
         console.log('run');
 
+        this.error = null;
+        this.output = null;
+
         const language = this.languageControl.value;
         const code = this.code;
 
-        this.http
-            .post('http://localhost:3001/execute', { language, code })
-            .subscribe({
-                next: (response: any) => (this.output = response.output),
-                error: (error) => console.error(error),
-            });
+        if (language && typeof language === 'string') {
+            const mockResponse = this.mockServerResponse(language, code);
+
+            if (mockResponse) {
+                if (mockResponse.status === 'success') {
+                    this.output = mockResponse.output || null;
+                } else if (
+                    mockResponse.status === 'error' &&
+                    'error' in mockResponse
+                ) {
+                    this.error = mockResponse.error;
+                }
+            } else {
+                this.error = 'Произошла ошибка при выполнении запроса.';
+            }
+        }
+    }
+
+    mockServerResponse(language: 'javascript' | 'python', code: string) {
+        const responses = {
+            javascript: [
+                {
+                    code: "console.log('Hello, world!');",
+                    status: 'success',
+                    output: 'Hello, world!\n',
+                },
+                {
+                    code: "console.log(Hello world!');",
+                    status: 'error',
+                    error: 'SyntaxError: Unexpected token',
+                },
+            ],
+            python: [
+                {
+                    code: "print('Hello, Python world!')",
+                    status: 'success',
+                    output: 'Hello, Python world!\n',
+                },
+            ],
+        };
+
+        if (responses[language]) {
+            const response = responses[language].find(
+                (item) => item.code === code
+            );
+            return response;
+        }
+
+        return null;
     }
 }
