6c91b20b4d2982a9e5af2d5a9674ec4db2ceea8b
Natalia Bandurova <natacsp@mail.ru>
2024-12-17 11:59:08 +0300

    chore: change monaco v

--- code-editor/src/app/code-editor/code-editor.component.ts
+++ code-editor/src/app/code-editor/code-editor.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     standalone: true,
     selector: 'app-code-editor',
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     templateUrl: './code-editor.component.html',
     styleUrl: './code-editor.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
